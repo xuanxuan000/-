8aea5091e73665c9180ab73be9e10fe8b69875ad
huahua <1430797759@qq.com>
2023-03-08 11:07:45 +0800

    add cfg.ini

--- main.cpp
+++ main.cpp
@@ -6,17 +6,29 @@
 #include "QTimer"
 #include "QElapsedTimer"
 #include "QThread"
-#include "qDebug"
+#include "QDebug"
+#include "QFile"
+#include "QMessageBox"
+#include "QProcess"
 
 void mySleep(int ms, bool isSync = false);
+const QString CfgFile = "./config.ini";
 
 int main(int argc, char *argv[])
 {
+    // 清理原程序
+    QString cmd;
+    cmd = QString("TASKKILL /F /IM %1 /T").arg(argv[0]);
+    QProcess p;
+    p.start("TASKKILL", QStringList()<< "/F" << "/IM" << argv[0] << "/T");
+    p.waitForStarted();
+    p.waitForFinished();
+
     QApplication a(argc, argv);
-    QLabel *label = new QLabel("起来走走，去喝点儿水。", nullptr);
+    QLabel *label = new QLabel("？？？", nullptr);
     label->setStyleSheet("color:green;\
         font-family:'Microsoft YaHei';\
-        font-size:18px;");
+        font-size:28px;");
     label->setAlignment(Qt::AlignCenter);
     QGridLayout* layout = new QGridLayout();
     layout->addWidget(label);
@@ -25,13 +37,30 @@ int main(int argc, char *argv[])
     w.setLayout(layout);
 //    w.showFullScreen();
 
+    int showTime = 5, elaspe = 50;
+    // 读取配置
+    QFile file(CfgFile);
+    if(file.open(QIODevice::ReadOnly)){
+        QString cfgData = file.readAll().replace("\n", "").replace(" ", "");
+        QStringList data = cfgData.split(",");
+        if(data.size() != 2){
+            QMessageBox::warning(nullptr, "warning", QString("%1 format err").arg(CfgFile));
+            return -1;
+        }
+        showTime = data[0].toInt();
+        elaspe = data[1].toInt();
+    }else{
+        QMessageBox::warning(nullptr, "warning", QString("can not found %1, use default cfg").arg(CfgFile));
+    }
+    QMessageBox::information(nullptr, "info", QString("show: %1min, elaspe: %2min").arg(showTime).arg(elaspe));
+
     QTimer *timer = new QTimer();
 //    QObject::connect(timer, &QTimer::timeout, &w, &Widget::showFullScreen);
     QObject::connect(timer, &QTimer::timeout, &w, [&](){
         qDebug() << "show";
         w.showFullScreen();
 //        mySleep(1000, true);
-        mySleep(3*60*1000, true);
+        mySleep(5*60*1000, true);
         w.hide();
     });
 //    timer->start(5*1000);
